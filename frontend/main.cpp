ca6cb9d19e6648bfbc952e8cb54f857e546caf55
Andrej Halveland <andrej.halv@gmail.com>
2026-09-12 19:35:45 +0200

    frontend: increase sleep time from 8ms to 33ms in the rendering logic

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1699,7 +1699,7 @@ int main(int argc, char** argv)
             }
             SDL_GL_SwapWindow(window);
         } else {
-            std::this_thread::sleep_for(std::chrono::milliseconds(8));
+            std::this_thread::sleep_for(std::chrono::milliseconds(33));
         }
 
         timer.render.end();
